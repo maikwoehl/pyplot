3306392b926dc0621828ab4162c28d9c1254f699
Maik Wöhl <maik.woehl@googlemail.com>
2013-05-04 11:02:38 +0200

    dynamic generated labels relative to given width, height and steps

--- main.py
+++ main.py
@@ -3,26 +3,19 @@
 import tkinter as tk
 import plotter
 
-# TODO:
-# - steps
-# -- calculate amount
-# -- generate positive and negative list
-
 def main():
     root = tk.Tk()
     root.title("Canvas")
     
-    canvas = tk.Canvas(root,width=500,height=500)
+    canvas = tk.Canvas(root,width=1000,height=1000)
     canvas.pack()
     
     exitBtn = tk.Button(root,text="Exit",command=exit)
     exitBtn.pack()
     
-    #makeGrid(canvas, 500,500)
-    #makeCross(canvas, 100,150)
-    
-    plot = plotter.Plotter(canvas,500,500)
+    plot = plotter.Plotter(canvas,1000,1000,50)
     plot.makeCross(100,150)
+    plot.makeLabels()
     
     root.mainloop()
     
--- plotter.py
+++ plotter.py
@@ -1,15 +1,51 @@
 #!/usr/bin/python3
 
 class Plotter:
-    def __init__(self,canvas,width,height):
+    def __init__(self,canvas,width,height,steps):
         self.w = canvas
         self.x = width
         self.y = height
+        self.steps = steps
+        self.xlabels = []
+        self.ylabels = []
+        
         self.makeGrid()
         
     def makeCross(self,x,y):
         self.w.create_line(x-5,y-5,x+5,y+6)
         self.w.create_line(x+5,y-6,x-5,y+5)
+        
+    def generateLabelsList(self):
+        amountx = int(((self.x/self.steps)/2)-1)
+        amounty = int(((self.y/self.steps)/2)-1)
+        labelsx = []
+        labelsy = []
+        
+        for i in range(amountx*2):
+            if i == 0:
+                labelsx.append("-"+str(amountx))
+            elif i < amountx:
+                labelsx.append("-"+str(amountx-i))
+            elif i == amountx:
+                labelsx.append(str(int(i/i)))
+            else:
+                labelsx.append(str(int(i-3)))
+                
+            
+        for i in range(amounty*2):
+            if i == 0:
+                labelsy.append(str(amounty))
+            elif i < amounty:
+                labelsy.append(str(amounty-i))
+            elif i == amounty:
+                labelsy.append("-"+str(int(i/i)))
+            else:
+                labelsy.append("-"+str(int(i-3)))
+                
+            
+        self.xlabels = labelsx
+        self.ylabels = labelsy
+        
         
     def makeGrid(self):
         # X-/Y-Axis
@@ -21,27 +57,24 @@ class Plotter:
         self.w.create_text(self.x-10,(self.y/2)+20,text="X",fill="purple")
     
         # Axis strikes and labels
-        i = 50
+        self.generateLabelsList()
+        i = self.steps
         label = 0
-        labely = self.y-50 # reverse i
-        labels=("-4","-3","-2","-1","1","2","3","4")
-        while (i<=self.x-50):
+        while (i<=self.x-self.steps):
             if i != self.x/2:
                 # Grid
                 self.w.create_line(0,i,self.x,i,fill="gray")
                 self.w.create_line(i,0,i,self.y,fill="gray")
             
                 # Labels
-                self.w.create_text(i,(self.y/2)+10,text=labels[label]) #X
-                self.w.create_text((self.x/2)-10,labely,text=labels[label]) #Y
+                self.w.create_text(i,(self.y/2)+10,text=self.xlabels[label]) #X
+                self.w.create_text((self.x/2)-10,i,text=self.ylabels[label]) #Y
             
             else:
                 label=label-1
         
             self.w.create_line(i,(self.y/2)-5,i,(self.y/2)+5) #X
             self.w.create_line((self.x/2)-5,i,(self.x/2)+5,i) #Y
-                        
-        
+            
+            i=i+self.steps
             label=label+1
-            i=i+50
-            labely=labely-50
